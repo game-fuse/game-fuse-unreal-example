be7f213976885ee72ded3fa818f4b321eac71373
Tom Giagtzoglou <tom.giagtzoglou@gmail.com>
2025-08-01 11:55:45 -0400

    update build settings, remove unused dependency

--- Source/EndlessRoadRacer.Target.cs
+++ Source/EndlessRoadRacer.Target.cs
@@ -8,7 +8,7 @@ public class EndlessRoadRacerTarget : TargetRules
 	public EndlessRoadRacerTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V5;
+		DefaultBuildSettings = BuildSettingsVersion.Latest;
 
 		ExtraModuleNames.AddRange( new string[] { "EndlessRoadRacer" } );
 	}
--- Source/EndlessRoadRacerEditor.Target.cs
+++ Source/EndlessRoadRacerEditor.Target.cs
@@ -8,7 +8,7 @@ public class EndlessRoadRacerEditorTarget : TargetRules
 	public EndlessRoadRacerEditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V5;
+		DefaultBuildSettings = BuildSettingsVersion.Latest;
 
 		ExtraModuleNames.AddRange( new string[] { "EndlessRoadRacer" } );
 	}
